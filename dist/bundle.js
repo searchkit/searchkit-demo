f2b649923423446e7f2a552c4682ff6d5854eeb6
Joseph McElroy <phoey1@gmail.com>
2015-12-09 21:12:16 +0000

    first commit

---
+++ dist/bundle.js
@@ -0,0 +1,1 @@
+!function(r){function t(o){if(n[o])return n[o].exports;var e=n[o]={exports:{},id:o,loaded:!1};return r[o].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}var n={};return t.m=r,t.c=n,t.p="/static/",t(0)}([function(r,t,n){!function(){throw new Error('Cannot find module "./src/index.tsx"')}()}]);
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {App} from "./app/src/App.tsx";
+
+import {SearchkitManager, history, SearchkitProvider} from "searchkit";
+import { Router, Route, Link, Redirect } from 'react-router'
+
+const searchkit = new SearchkitManager("aetna")
+
+
+class Root extends React.Component<any, any> {
+	render(){
+		return (
+			<SearchkitProvider searchkit={searchkit}>
+				<App/>
+			</SearchkitProvider>
+		)
+	}
+}
+
+
+ReactDOM.render((
+	<Router history={history}>
+		<Route path="/movies-app" component={Root}/>
+	</Router>
+), document.getElementById('root'))
