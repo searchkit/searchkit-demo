1c6ab2cca582a704e2a6da98b57727ee653cc2e9
Ash <ssetem@googlemail.com>
2015-12-24 15:06:29 +0000

    fix index

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import * as ReactDOM from "react-dom";
 import {App} from "./app/src/App.tsx";
 
 import {
-	SearchkitManager, SearchkitProvider
+  SearchkitManager, SearchkitProvider
 } from "searchkit";
 
 const searchkit = new SearchkitManager("/", {multipleSearchers:false})
 
 ReactDOM.render((
-	<SearchkitProvider searchkit={searchkit}>
-		<App/>
-	</SearchkitProvider>
+  <SearchkitProvider searchkit={searchkit}>
+    <App/>
+  </SearchkitProvider>
 ),  document.getElementById('root'))
