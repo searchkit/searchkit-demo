22538b33cbdfe49bc8bc4444f5ae02dd212585b6
Gregory Potdevin <gregory.potdevin@gmail.com>
2016-02-29 16:54:20 +0100

    remove dead code

--- src/app/src/playground/ui/FilterItemList.tsx
+++ src/app/src/playground/ui/FilterItemList.tsx
@@ -4,52 +4,11 @@ import { FilterItemComponent } from "./FilterItem"
 
 const block = require('bem-cn')
 
-const defaults = require("lodash/defaults")
 const map = require("lodash/map")
 const includes = require("lodash/includes")
 
 require('./FilterItemList.scss');
 
-
-// hasOptions():boolean {
-//   return this.props.buckets.length != 0
-// }
-
-// render() {
-
-//   const { id, title, bemBlocks, buckets } = this.props
-
-//   let block = bemBlocks.container
-//   let className = block()
-//     .mix(`filter--${id}`)
-//     .state({
-//       disabled: !this.hasOptions()
-//     })
-
-//   return (
-//     <div data-qa={`filter--${this.props.id}`} className={className}>
-//         <div data-qa="header" className={block("header") }>{title}</div>
-//         <div data-qa="options" className={block("options") }>
-//           {map(buckets, this.renderOption.bind(this)) }
-//           </div>
-//         {this.renderShowMore() }
-//       </div>
-//   );
-// }
-
-// renderOption(option) {
-//   const { itemComponent, bemBlocks, state, toggleFilter, translate } = this.props
-
-//   return React.createElement(itemComponent, {
-//     key: option.key,
-//     label: translate(option.key),
-//     count: option.doc_count,
-//     selected: state.contains(option.key),
-//     translate,
-//     bemBlocks,
-//     toggleFilter: () => toggleFilter(option.key)
-//   });
-// }
 
 
 export class FilterItemList extends React.Component<any, {}> {
