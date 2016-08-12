30a7a4b5ee2a5427fd18a9471033436acf3bcb89
barantaran <yourchev@gmail.com>
2016-08-12 17:50:28 +0300

    Add range filter

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -67,8 +67,8 @@ export class App extends React.Component<any, any> {
       			<SideBar>
       				<HierarchicalMenuFilter fields={["type.raw", "genres.raw"]} title="Categories" id="categories"/>
               <RangeFilter min={0} max={100} field="metaScore" id="metascore" title="Metascore" showHistogram={true}/>
-              <RangeFilter min={0} max={10} field="imdbRating" id="imdbRating" title="IMDB Rating" showHistogram={true}/>
-              <RefinementListFilter id="actors" title="Actors" field="actors.raw" size={10}/>
+              <RangeFilter min={0} max={3000} field="exifimagelength" id="exifimagelength" title="Image Length" showHistogram={true}/>
+              <RangeFilter min={0} max={3000} field="exifimagewidth" id="exifimagewidth" title="Image Width" showHistogram={true}/>
               <RefinementListFilter id="author" title="Author" field="author.raw" size={10}/>
       				<RefinementListFilter translations={{"facets.view_more":"View more writers"}} id="writers" title="Writers" field="writers.raw" operator="OR" size={10}/>
       				<RefinementListFilter id="countries" title="Countries" field="countries.raw" operator="OR" size={10}/>
